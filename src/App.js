46fdfb70ff2a869a213ce2da309f3a0b0c91671c
Hiroki Kinoshita <hiroki.kinoshita@sun-asterisk.com>
2019-05-28 14:18:05 +0700

    first-commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,61 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+export class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      text: '',
+      inputValue: '',
+    }
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleChange(value) {
+    this.setState({
+      inputValue: value,
+    });
+  }
+
+  handleClick() {
+    this.setState({
+      text: this.state.inputValue,
+      inputValue: '',
+    });
+  }
+
+  render() {
+    return (
+      <div className={this.state.text}>
+        <Title text={this.state.text} />
+        <Input handleChange={this.handleChange} value={this.state.inputValue} />
+        <Button handleClick={this.handleClick} />
+      </div>
+    );
+  }
+}
+
+export const Title = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1>Hello {props.text}</h1>
   );
 }
 
-export default App;
+export const Input = (props) => {
+  function handleChange(event) {
+    props.handleChange(event.target.value);
+  }
+
+  return (
+    <input onChange={handleChange} value={props.value} />
+  );
+}
+
+export const Button = (props) => {
+  const handleClick = () => {
+    props.handleClick();
+  }
+  return (
+    <button onClick={handleClick}>送信</button>
+  );
+}
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,105 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+//import ReactDOM from 'react-dom';
+import { App, Title, Input, Button } from './App';
+import { shallow } from 'enzyme';
+import renderer from 'react-test-renderer';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
+/**
+ * Appコンポーネントのテスト
+ */
+describe('<App />', () => {
+  it('子コンポーネントが存在すること', () => {
+    // == 準備 ==
+    /** Appコンポーネントをshallowレンダリング */
+    const wrapper = shallow(<App />);
+    //子コンポーネントは、shallowレンダリングしたwrapper変数を使ってwrapper.find(子コンポーネント名)で取得可能です。ここでは、lengthを使った個数での存在チェックを行なっています。
+    // == 検証 ==
+    /** 各コンポーネントの数を取得し、1であればOK */
+    expect(wrapper.find(Title).length).toBe(1);
+    expect(wrapper.find(Input).length).toBe(1);
+    expect(wrapper.find(Button).length).toBe(1);
+  });
+
+  it('this.state.textを更新した時にclass名に反映される事', () => {
+    // == 準備 ==
+    /** Appコンポーネントをshallowレンダリング */
+    const wrapper = shallow(<App />);
+
+    // == 実行 ==
+    /** setStateしてthis.state.textの値を'XXX'に更新 */
+    wrapper.setState({
+      text: 'XXX',
+    });
+    // == 検証 ==
+    /** 'XXX'というclass名を持った要素があればOK */
+    expect(wrapper.find('.XXX').length).toBe(1);
+  });
+
+  it('handleClickを呼び出すと、setStateが呼び出される', () => {
+    // == 準備 ==
+    /** Appコンポーネントをshallowレンダリング */
+    const wrapper = shallow(<App />);
+    //setStateをスパイ化
+    const setStateSpy = jest.spyOn(App.prototype, 'setState');
+    //setStateしてthis.state.inputValueの値を'XXX'に更新
+    wrapper.setState({
+      inputValue: 'XXX',
+    });
+    //handleClick()を呼び出す
+    wrapper.instance().handleClick();
+    //適切な引数でspy化したsetStateが呼び出されていればOK
+    expect(setStateSpy).toHaveBeenCalledWith({
+      //以下の引数とともにチェックを行なっている
+      text: 'XXX',
+      inputValue: '',
+    });
+
+    it('<App />のスナップショット', () => {
+      const tree = renderer
+        .create(<App />)
+        .toJSON();
+      expect(tree).toMatchSnapshot();
+    });
+  });
+
+  /**
+   * Titleコンポーネントのテスト
+   */
+  describe('<Title />', () => {
+    it('受け取ったpropsの値を表示する事', () => {
+      /**
+     * 'React'という値をtextに渡して、
+     * Titleコンポーネントをshallowレンダリング
+     */
+      const wrapper = shallow(<Title text={'React'} />);
+      // == 検証 ==
+      /** レンダリングされたテキストが'Hello React'であればOK */
+      expect(wrapper.text()).toBe('Hello React');
+      //==実行
+      //props.textの値を'Worldに変更
+      wrapper.setProps({ text: 'World' });
+      //==検証==
+      //レンダリングされたテキストがHello WorldであればOK
+      expect(wrapper.text()).toBe('Hello World');
+    });
+  })
+
+  describe('<Input />', () => {
+    it('changeイベント発火時にコールバック関数が呼び出される事', () => {
+      //準備
+      //mock関数としてhandleChangeSpyを作成
+      //親コンポーネントからもらうmethodは
+      const handlechangeSpy = jest.fn();//jest.fin()を使ってmock関数として作成します。
+      //mock関数handleChangeSpyを渡して、Inputコンポーネントをshalloレンダリング
+      const wrapper = shallow(< Input handelChange={handlechangeSpy} />);
+      //ダミーなeventオブジェクトを作成
+      const event = { target: { value: 'XXX' } };
+      //実行
+      //input要素に対してchangeイベントを発火させる
+      wrapper.find('input').simulate('change', event);//イベントを発火させる為にsimulate()メソッドを用います//その際にeventメソッドを作成して渡しています。
+      //検証
+      //mock関数'handleChangeSpy'がXXXという引数で呼び出さればOK
+      expect(handlechangeSpy).toHaveBeenCalledWith('XXX');
+    });
+  })
+})
